cc9e19336dee90259df73d977431e88444d82e3a
arifamsar <dcylert@gmail.com>
2025-06-28 02:28:58 +0700

    chore: comment out @nuxt/scripts module in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     // '@nuxt/fonts',
     // '@nuxt/icon',
     // '@nuxt/image',
-    '@nuxt/scripts',
+    // '@nuxt/scripts',
     'shadcn-nuxt',
     '@nuxtjs/supabase',
   ],
